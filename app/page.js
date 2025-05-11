b42f6ee65937517200904928ee7125da00934645
Jella Calupit <jellamaecalupit2003@gmail.com>
2025-05-12 00:27:30 +0800

    refactor: Replace anchor tag with Link component and add logo image for improved navigation and branding

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import GetStartedButton from "./components/GetStartedButton";
 import Image from "next/image";
+import logoImage from "../public/globe.svg";
 
 export default function Home() {
   return (
@@ -8,12 +9,18 @@ export default function Home() {
       <header className="w-full">
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="#" className="flex items-center">
-              <span className="mr-3 h-6 sm:h-9">Logo</span>
+            <Link href="/" className="flex items-center">
+              <Image
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="mr-3 h-6 sm:h-9"
+                alt="Flowbite Logo"
+                width={36}
+                height={36}
+              />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 INFO SITE
               </span>
-            </a>
+            </Link>
             <div className="flex items-center lg:order-2">
               <a
                 href="/login"
