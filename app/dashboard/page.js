dd8f294a8d1bb4353e14a928b8cd5679a7d6a746
Jella Calupit <jellamaecalupit2003@gmail.com>
2025-05-11 22:58:29 +0800

    refactor: Replace anchor tags with Link components for navigation

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -145,16 +145,18 @@ export default function Home() {
       <header className="w-full">
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" className="flex items-center">
-              <img
+            <Link href="/" className="flex items-center">
+              <Image
                 src="https://flowbite.com/docs/images/logo.svg"
                 className="mr-3 h-6 sm:h-9"
                 alt="Flowbite Logo"
+                width={36}
+                height={36}
               />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 INFO SITE
               </span>
-            </a>
+            </Link>
 
             <div
               className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
@@ -195,12 +197,12 @@ export default function Home() {
                   </Link>
                 </li>
                 <li>
-                  <a
+                  <Link
                     href="/about"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     About Us
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
